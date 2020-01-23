57cddfc08b402bb843883e4b3f17558e3bab4efe
Alexa Perez <alexapg8@gmail.com>
2020-01-23 11:06:23 -0500

    initial commit

---
+++ Lab-3-unittest-function.js
@@ -0,0 +1,18 @@
+//LAB 3 - UNIT TESTING
+//======== THIS FILE IS FOR THE checkHumbrId FUNCTION ==========
+// alert("lab 3 Function");//please delete this line once connected.
+/** checkHumbrId Function
+ * Validate input as a Humber College Student number.
+ * Returns true if input validates.
+ * @param {string} idIn
+ */
+function checkHumbrId(humberIn) {
+	var RegExHumberId = /^(n|N)\d{8}/;
+	
+	let validHumbrId = false;
+	if (RegExHumbrId.test(humberIn)){
+		validHumbrId = true;
+	}
+	
+	return validHumbrId;
+}
